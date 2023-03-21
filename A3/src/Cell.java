3f32cf2f88919c1d3b52e76df20cb5da79496e17
Administrator <1303504545@qq.com>
2023-03-21 00:08:46 +0000

    create new class cell

---
+++ A3/src/Cell.java
@@ -0,0 +1,10 @@
+/**
+ * @BelongsProject: A3lab
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: 220010835
+ * @CreateTime: 21-03-2023  00:07
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Cell {
+}
